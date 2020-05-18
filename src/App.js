ff1f8f9d666a0fc70fe1adb59ab13ec2ae7eeda0
Oghenetega Peter-Saduwa <Oghenetega.peter@wemabank.com>
2020-05-18 02:45:33 +0100

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Homepage from "./Homepage.component";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
