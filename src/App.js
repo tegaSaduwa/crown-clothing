bd6d7d4ac93edb627e9f38820a4fb03465216d78
Oghenetega Peter-Saduwa <Oghenetega.peter@wemabank.com>
2020-06-10 01:11:48 +0100

    update our menu-item to navigate with withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Homepage from "./Homepage.component";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/hompage/Homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/hats" component={HatsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/hompage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component.jsx";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
